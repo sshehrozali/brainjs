dc41275bced046fa6d4123e9f8beba72b43d2b5f
Shehroz Ali <sshehroza13@gmail.com>
2021-05-16 14:29:03 +0500

    Updated training data

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ const persontrainingData = persondata.map(item => ({
 network.train(persontrainingData, {
   iterations: 500
 });
-var textMsg = "i love you so much"; // Person's input
+var textMsg = "looks like you are cooking?"; // Person's input
 const PersonOutput = network.run(textMsg);  // Build person model
 
 
 // Train Replier Model
-const repliertrainingData = replierdata.map(item => ({
-  input: item.expectations,
-  output: item.reply
-}))
+// const repliertrainingData = replierdata.map(item => ({
+//   input: item.expectations,
+//   output: item.reply
+// }))
 
-network.train(repliertrainingData, {
-  iterations: 500
-});
-const ReplierOutput = network.run(PersonOutput);  // Build replier model
+// network.train(repliertrainingData, {
+//   iterations: 500
+// });
+// const ReplierOutput = network.run(PersonOutput);  // Build replier model
 
 
 
@@ -38,4 +38,4 @@ const ReplierOutput = network.run(PersonOutput);  // Build replier model
 // See results
 console.log(`Msg: ${textMsg}`);
 console.log(`Expectation: ${PersonOutput}`);
-console.log(`Reply: ${ReplierOutput}`);
+// console.log(`Reply: ${ReplierOutput}`);
